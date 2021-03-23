6d3f045d0606a51c5f20abe956120b1400557e6c
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-03-23 16:10:01 -0500

    typescript generic object

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    if (showResult) {
-        var result_1 = n1 + n2;
-        console.log(phrase + result_1);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2;
-var printResult = true;
-var resultPhrase = 'Result Is: ';
-var result = add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'Sam',
+    age: 30
+};
+console.log(person.nickname);
--- app.ts
+++ app.ts
@@ -1,16 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string){
-  if(showResult){
-    //saving the result of addition inside another variable so that Javascript doesn't concat the string and number types to result in the answer being "52" and not 7
-    const result = n1 + n2
-    console.log(phrase + result)
-  } else{
-    return n1 + n2;
-  }
-}
+const person = {
+  name: 'Sam',
+  age: 30
+};
 
-const number1 = 5;
-const number2 = 2;
-const printResult = true;
-const resultPhrase = 'Result Is: ';
-
-const result = add(number1, number2, printResult, resultPhrase)
+console.log(person.nickname)
