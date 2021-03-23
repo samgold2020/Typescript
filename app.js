f5912de84131c4890a948895028c8e353542188c
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-03-23 15:53:25 -0500

    typescript number

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        var result_1 = n1 + n2;
+        console.log(phrase + result_1);
+    }
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = "5";
+var number1 = 5;
 var number2 = 2;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result Is: ';
+var result = add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
-function add(n1: number, n2: number){
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string){
+  if(showResult){
+    //saving the result of addition inside another variable so that Javascript doesn't concat the string and number types to result in the answer being "52" and not 7
+    const result = n1 + n2
+    console.log(phrase + result)
+  } else{
+    return n1 + n2;
+  }
 }
 
-const number1 = "5";
+const number1 = 5;
 const number2 = 2;
+const printResult = true;
+const resultPhrase = 'Result Is: ';
 
-const result = add(number1, number2)
-console.log(result)
+const result = add(number1, number2, printResult, resultPhrase)
